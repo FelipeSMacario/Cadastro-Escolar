27a90493830c467b9872f83686a8738e0a3e7f6f
bruno.machado <bruno.machado@atos.net>
2023-02-23 18:17:19 -0300

    feat: add send email with notas to aluno

--- src/main/java/com/escola/cadastro/escolar/service/NotasService.java
+++ src/main/java/com/escola/cadastro/escolar/service/NotasService.java
@@ -12,7 +12,16 @@ import com.escola.cadastro.escolar.repository.MateriaRepository;
 import com.escola.cadastro.escolar.repository.NotasRepository;
 import com.escola.cadastro.escolar.repository.PessoaRepository;
 import com.escola.cadastro.escolar.repository.TurmaRepository;
-import org.aspectj.weaver.ast.Not;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.io.IOException;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,6 +45,52 @@ public class NotasService {
     @Autowired
     TurmaRepository turmaRepository;
 
+    public void sendEmailNotasToUser(String email, String name, String nota, String materia) throws java.io.IOException {
+
+        System.out.println("Sending nota email to " + email);
+
+        Dotenv dotenv = Dotenv.load();
+
+        Email from = new Email("brunofonseca821@gmail.com");
+        String subject = "Cadastro realizado";
+        Email to = new Email(email);
+        Content content = new Content("text/html", " ");
+
+        Mail mail = new Mail(from, subject, to, content);
+        
+        Personalization personalization0 = new Personalization();
+        personalization0.addDynamicTemplateData("name", name);
+        personalization0.addDynamicTemplateData("email", email);
+        personalization0.addDynamicTemplateData("nota", nota);
+        personalization0.addDynamicTemplateData("materia", materia);
+
+        personalization0.addTo(new Email(email));
+
+        mail.addPersonalization(personalization0);
+
+        mail.setTemplateId("d-c810e44a8a484641aa82cf572f750387");
+
+        SendGrid sg = new SendGrid(dotenv.get("SENDGRID_API_KEY"));
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+
+            System.out.println("Email to user " + email + " sent successfully");
+
+        } catch (IOException ex) {
+            System.out.println("Error when trying to send email to:" + email);
+            throw ex;
+        }
+       
+    }
+
     public ResponseEntity cadastrarNotas(NotasDTO notasDTO) {
         Optional<Pessoa> professor = buscaPessoa(notasDTO.getMatriculaProfessor(), "Professor");
         Optional<Materia> materia = buscaMateria(notasDTO.getMateria());
@@ -44,7 +99,7 @@ public class NotasService {
         notasDTO.getMatriculasNotas().forEach(valor -> {
             Optional<Pessoa> aluno = buscaPessoa(valor.getMatriculaAluno(), "Aluno");
             validaTrimeste(professor.get().getMatricula(), aluno.get().getMatricula(), materia.get().getId(), buscaTrimeste(), notasDTO.getTurmaId());
-            Notas motas = Notas.builder()
+            Notas notas = Notas.builder()
                     .nota(valor.getNotas())
                     .professor(professor.get())
                     .turma(turma.get())
@@ -52,7 +107,14 @@ public class NotasService {
                     .materia(materia.get())
                     .dataInclusao(LocalDate.now())
                     .trimestre(buscaTrimeste()).build();
-            notasRepository.save(motas);
+            notasRepository.save(notas);
+
+            try {
+                sendEmailNotasToUser(aluno.get().getEmail(), aluno.get().getNome(), valor.getNotas().toString(), materia.get().getNome());
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+
         });
 
         return ResponseEntity.ok().body(notasDTO);
